55dac1b0c6699d9779720bd54d9df980af0eb060
GitVex <52197394+GitVex@users.noreply.github.com>
2023-01-17 12:26:30 +0000

    UI & UX improvements reduced the space that a ViewerContainer takes up added controls for the ammount of results shown

--- components/dbForm/Viewer.tsx
+++ components/dbForm/Viewer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useQuery } from 'react-query'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import ViewerContainer from './ViewerContainer'
 import { useFilterState, usePingRefetch } from '../contexts/FilterStateProvider'
 import TrackTotal from './TrackTotal'
@@ -8,6 +8,7 @@ import TrackTotal from './TrackTotal'
 
 function Viewer() {
 
+    const [maxResults, setMaxResults] = useState(7)
     const filterState = useFilterState()
     const ping = usePingRefetch()
 
@@ -22,7 +23,7 @@ function Viewer() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ filter: filterState })
+            body: JSON.stringify({ filter: filterState, maxResults: maxResults })
         })
         const res = await response.json()
         return res
@@ -32,23 +33,31 @@ function Viewer() {
 
     return (
         <>
+            <div className='flex flex-col gap-2 items-center w-fit'>
+                <h1>Viewer</h1>
 
-            <h1>Viewer</h1>
-            {
-                isLoading ? (
-                    <div>Loading...</div>
-                ) : error ? (
-                    //@ts-ignore 
-                    <div>Error: {error.message}</div>
-                ) : data ? (
-                    <div className='flex flex-col gap-2 items-center w-full'>
-                        {data.map((track: any) => (
-                            <ViewerContainer key={track.id} id={track.id} title={track.title} artist={track.artist} url={track.url} tags={track.tags} />
-                        ))}
-                    </div>
-                ) : null
-            }
-            <TrackTotal />
+                {
+                    isLoading ? (
+                        <div>Loading...</div>
+                    ) : error ? (
+                        //@ts-ignore 
+                        <div>Error: {error.message}</div>
+                    ) : data ? (
+                        <div className='flex flex-col gap-1 items-center w-full'>
+                            {data.map((track: any) => (
+                                <ViewerContainer key={track.id} id={track.id} title={track.title} artist={track.artist} url={track.url} tags={track.tags} />
+                            ))}
+                        </div>
+                    ) : null
+                }
+                <TrackTotal />
+                <button onClick={() => refetch()} className='p-2 rounded bg-gray-800/50 w-fit hover:bg-gray-800/80 duration-100'>Refresh</button>
+                <span className='flex flex-row gap-2 items-center'>
+                    <button onClick={() => setMaxResults(maxResults - 2)} className='p-2 rounded bg-gray-800/50 w-fit hover:bg-gray-800/80 duration-100'>Load less</button>
+                    <p>[showing: {maxResults}]</p>
+                    <button onClick={() => setMaxResults(maxResults + 2)} className='p-2 rounded bg-gray-800/50 w-fit hover:bg-gray-800/80 duration-100'>Load more</button>
+                </span>
+            </div>
         </>
 
     )
--- components/dbForm/ViewerContainer.tsx
+++ components/dbForm/ViewerContainer.tsx
@@ -7,18 +7,18 @@ function ViewerContainer(props: any) {
     const artist = props.artist
     const url = props.url
     const tags = props.tags
-    const i = tags.map((tag: any) => tag.name + ", ")
+    const i = tags.map((tag: any) => tag.name).join(', ')
 
     return (
-        <div className='bg-gray-800/50 p-2 rounded grow w-full md:w-5/6'>
+        <div className='bg-gray-800/50 p-1 rounded grow w-full md:w-5/6'>
             <div className='flex flex-row items-center gap-2 w-full'>
                 <p className='opacity-50'>{id}</p>
                 <p onClick={() => { window.open(url, '_blank') }} className='hover:cursor-pointer truncate'>{title}</p>
                 <p className='grow opacity-50 truncate'>{artist}</p>
                 <button onClick={() => { navigator.clipboard.writeText(url) }} className='bg-gray-800/50 hover:bg-gray-800/25 rounded p-1 grow-0'>Copy</button>
             </div>
-            <div className='flex flex-row items-center justify-start gap-2'>
-                <p className='opacity-50 truncate'>{i}</p>
+            <div className='flex flex-row items-center justify-start gap-2 text-sm'>
+                <p className='opacity-50'>{i}</p>
             </div>
         </div>
     )
