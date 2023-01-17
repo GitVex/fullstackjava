b0cdf657cc3746b6a979ecf6bcf9d970f27b1d91
GitVex <52197394+GitVex@users.noreply.github.com>
2023-01-17 08:59:28 +0000

    fixed addition affirmator position and animation

--- components/utils/Affirmator.tsx
+++ components/utils/Affirmator.tsx
@@ -5,20 +5,25 @@ import { motion } from 'framer-motion'
 const Affirmator = (props: { affirmState: any, setAffirmState: any}) => {
 
     const { affirmState, setAffirmState } = props
+    const debug = false
 
     React.useEffect(() => {
         if (affirmState) {
             setTimeout(() => {
                 setAffirmState(false)
             }, 1500)
+        } else if (debug && !affirmState) {
+            setTimeout(() => {
+                setAffirmState(true)
+            }, 1500)
         }
     }, [affirmState])
 
     return (
         <motion.div
-            animate={affirmState ? { scale: 1.5, opacity: 1, y: -10 } : { scale: 0, opacity: 0, y: 0 }}
-            transition={{ duration: 0.5 }}
-            className='x-5 w-5 h-5 bg-green-500 rounded-full flex text-justify justify-center items-center text-white text-sm font-bold'
+            animate={affirmState ? { scale: 1.5, opacity: 1} : { scale: 0, opacity: 0}}
+            transition={{ duration: 0.5, type: 'spring'  }}
+            className='x-5 w-5 h-5 bg-green-500 rounded-full flex text-justify justify-center items-center text-white text-sm font-bold origin-center'
         > 
             <p className='translate-y-[-0.1rem]'>+</p>
         </motion.div>
