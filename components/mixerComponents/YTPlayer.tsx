902e2ef0f4cab923016f8dfa774e06e81249ef3f
GitVex <52197394+GitVex@users.noreply.github.com>
2023-02-09 17:35:41 +0000

    dev fix

--- components/mixerComponents/YTPlayer.tsx
+++ components/mixerComponents/YTPlayer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import ts from 'typescript'
 import { usePlayerHolderById } from '../contexts/PlayerHolderProvider'
 
 const fadeInterval = 100
@@ -37,7 +38,7 @@ const fadeOut = (player: any, ID: number) => {
 // Youtube Player utilizing the Youtube IFrame API
 function YTPlayer({ playerId }: { playerId: number }) {
 
-    const player = usePlayerHolderById(playerId) ? usePlayerHolderById(playerId).player : null
+    const player = usePlayerHolderById(playerId).player
 
     const ID = `player${playerId}`
 
@@ -48,6 +49,7 @@ function YTPlayer({ playerId }: { playerId: number }) {
                 {/* Style an input slider that controls the players volume */}
                 <input id={`volumeSlider${ID}`} type="range" min="0" max="100" step="1" className="h-5/6 w-full bg-gray-800/50" orient="vertical" onChange={
                     (e) => {
+                        //@ts-ignore
                         player.setVolume(e.target.value)
                     }
                 } />
@@ -55,8 +57,11 @@ function YTPlayer({ playerId }: { playerId: number }) {
             <div className="flex flex-row justify-center items-center gap-2">
                 <input type="text" className="w-1/2 bg-gray-800/50 rounded p-1" placeholder="Video ID" onKeyDown={
                     (e) => {
+                        //@ts-ignore
                         if (e.key === 'Enter' && e.target.value) {
+                            //@ts-ignore
                             player.loadVideoById(e.target.value)
+                            //@ts-ignore
                             player.pauseVideo()
                         }
                     }
