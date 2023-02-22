7082036ebd2cdf2b53b4a120b86e187fac09df4d
GitVex <52197394+GitVex@users.noreply.github.com>
2023-02-22 09:53:04 +0000

    moved redundant "mixer" and "test" pages to siding

--- siding/test.tsx
+++ siding/test.tsx
@@ -1,28 +1,10 @@
 import React from 'react'
-import LoadingAnim from '../components/utils/LoadingAnimDismount'
-import { useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import DualInterface from '../components/DualInterface'
 
 function test() {
-
-	const [isPresent, setIsPresent] = useState(false)
-
-	return (
-		<div className="h-screen w-screen flex flex-col items-center place-content-around">
-			<div className='h-12'>
-				<AnimatePresence>
-					{isPresent ? (
-						<motion.div key='loader'>
-							<LoadingAnim />
-						</motion.div>
-					) : null}
-				</AnimatePresence>
-			</div>
-			<button onClick={() => setIsPresent(!isPresent)} className="button-primary">Toggle</button>
-			<p>isPresent: {isPresent.toString()}</p>
-
-		</div >
-	)
+  return (
+    <DualInterface />
+  )
 }
 
 export default test
