3a8da0bddf369aaa188aff504910b28284eb1ddd
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-14 07:24:51 +0000

    worked on ListItem

--- components/rebuiltComponents/Viewer/ListItem.tsx
+++ components/rebuiltComponents/Viewer/ListItem.tsx
@@ -6,14 +6,14 @@ function ListItem({ item }: { item: track }) {
 		<div className='flex flex-row gap-2 text-sm'>
 			<p
 				onClick={() => window.open(item.url, '_blank')?.focus()}
-				className='cursor-pointer'
+				className='cursor-pointer bg-slate-500/50 rounded'
 			>
 				{item.title}: <em>{item.artist}</em>
 			</p>
-			<button>
+			<button className='bg-slate-500/50 rounded'>
 				<p>Copy</p>
 			</button>
-			<button>
+			<button className='bg-slate-500/50 rounded'>
 				<p>Add</p>
 			</button>
 		</div>
