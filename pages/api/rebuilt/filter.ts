cfda6a98ae545850427254727be1e2f77eefe522
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-14 08:29:40 +0000

    Refactor API endpoints to optimize querying and ordering.
    In `filter.ts`, modify query generation logic to generate `where` clause based on `filter` input, then update the database query to only include `tags`.
    
    In `list.ts`, list all items and randomize the order of results.

---
+++ pages/api/rebuilt/filter.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "./prismaClientProvider";
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+
+    // Destructure filter and maxResults from req.body
+    const { filter = [] } = req.body;
+
+    // Generate the where clause based on the filter
+    const where = filter.length > 0
+        ? {
+            AND: filter.map((tag: string) => ({ tags: { some: { name: tag } } })),
+        }
+        : {};
+
+    // Query the database
+    const result = await prisma.track.findMany({
+        where: where,
+        include: {
+            tags: true,
+        },
+    });
+
+    res.json(result);
+}
+
+
