22e0bfed04ddc3c5a004fbca87afa5d761904936
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-14 08:10:53 +0000

    refactor ListItem

--- components/rebuiltComponents/Viewer/ListItem.tsx
+++ components/rebuiltComponents/Viewer/ListItem.tsx
@@ -3,7 +3,7 @@ import { track } from '@prisma/client';
 
 function ListItem({ item }: { item: track }) {
 	return (
-		<div className='flex w-full flex-row items-center gap-2 rounded-lg bg-slate-500/10 p-2 text-sm'>
+		<div className='flex w-full flex-row items-center gap-2 rounded-lg bg-indigo-600/10 p-2 text-sm'>
 			<div className='flex flex-1 flex-col w-full'>
 				<div
 					onClick={() => window.open(item.url, '_blank')?.focus()}
@@ -20,11 +20,11 @@ function ListItem({ item }: { item: track }) {
 			</div>
 			<button
 				onClick={() => navigator.clipboard.writeText(item.url)}
-				className='rounded bg-slate-500 px-2 py-1 text-xs font-semibold text-white'
+				className='rounded bg-indigo-600/20 px-2 py-1 text-xs font-semibold text-white'
 			>
 				Copy
 			</button>
-			<button className='ml-2 rounded bg-slate-500 px-2 py-1 text-xs font-semibold text-white'>
+			<button className='ml-2 rounded bg-indigo-600/20 px-2 py-1 text-xs font-semibold text-white'>
 				Add
 			</button>
 		</div>
