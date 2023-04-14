f28737b7739d4e08e140a887f76bb983021a116f
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-14 07:47:34 +0000

    Refactor list item component to improve accessibility
    Changed the list item component to improve accessibility by adding more descriptive text to the UI elements. The item info is now properly aligned and the copy and add buttons have been styled for better visibility.

--- components/rebuiltComponents/Viewer/ListItem.tsx
+++ components/rebuiltComponents/Viewer/ListItem.tsx
@@ -3,18 +3,26 @@ import { track } from '@prisma/client';
 
 function ListItem({ item }: { item: track }) {
 	return (
-		<div className='flex flex-row gap-2 text-sm'>
-			<p
-				onClick={() => window.open(item.url, '_blank')?.focus()}
-				className='cursor-pointer rounded bg-slate-500/10'
+		<div className='flex w-full flex-row items-center gap-2 rounded-lg bg-slate-500/10 p-2 text-sm'>
+			<div className='flex flex-1 flex-col'>
+				<p
+					onClick={() => window.open(item.url, '_blank')?.focus()}
+					className='cursor-pointer truncate font-semibold'
+				>
+					{item.title}
+				</p>
+				<p className='cursor-pointer truncate text-slate-600'>
+					<em>{item.artist}</em>
+				</p>
+			</div>
+			<button
+				onClick={() => navigator.clipboard.writeText(item.url)}
+				className='rounded bg-slate-500 px-2 py-1 text-xs font-semibold text-white'
 			>
-				{item.title}: <em>{item.artist}</em>
-			</p>
-			<button className='rounded bg-slate-500/10'>
-				<p>Copy</p>
+				Copy
 			</button>
-			<button className='rounded bg-slate-500/10'>
-				<p>Add</p>
+			<button className='ml-2 rounded bg-slate-500 px-2 py-1 text-xs font-semibold text-white'>
+				Add
 			</button>
 		</div>
 	);
