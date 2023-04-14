66522d0d9390a9eecc44c5163026a6f08772fe53
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-14 09:37:57 +0000

    Fixed default value for type in `ViewColumn`

---
+++ components/rebuiltComponents/Viewer/ViewColumn.tsx
@@ -0,0 +1,77 @@
+// ViewColumn.tsx
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { track } from '@prisma/client';
+import ListItem from './ListItem';
+
+export function ViewColumn({
+	children,
+	className,
+	style,
+	type = 'list',
+}: {
+	children?: React.ReactNode;
+	className?: string;
+	style?: React.CSSProperties;
+	type?: string;
+}) {
+	let route = '/api/rebuilt/list';
+	if (type === 'new') {
+		route = '/api/rebuilt/new';
+	} else if (type === 'trend') {
+		route = '/api/rebuilt/list';
+	} else if (type === 'filter') {
+		route = '/api/rebuilt/filter';
+	} else if (type === 'owned') {
+		route = '/api/rebuilt/list';
+	}
+
+	const { isLoading, error, data } = useQuery<track[], Error>(
+		['items', type],
+		fetchData,
+		{
+			enabled: true,
+			refetchInterval: 1000 * 60 * 20,
+		}
+	);
+
+	useEffect(() => {}, [data]);
+
+	async function fetchData(): Promise<track[]> {
+		const response = await fetch(route, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({}),
+		});
+		const res = await response.json();
+
+		return res;
+	}
+
+	return (
+		<div className={className} style={style}>
+			<p className='my-2 w-full text-center capitalize underline'>
+				{type}
+			</p>
+			<ul className='m-2 flex max-h-full flex-col gap-2'>
+				{isLoading ? (
+					<p>Loading...</p>
+				) : error ? (
+					<p>Error: {error.message}</p>
+				) : data ? (
+					data.map((item) => {
+						return (
+							<li key={item.id}>
+								<ListItem item={item} />
+							</li>
+						);
+					})
+				) : (
+					<p>no data</p>
+				)}
+			</ul>
+		</div>
+	);
+}
