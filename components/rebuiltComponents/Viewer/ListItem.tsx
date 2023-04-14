9f78e7b7dd7f03cc54ed43bef477112216c9b74c
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-14 07:32:27 +0000

    Refactor ListItem component styles for consistency and improved readability.

--- components/rebuiltComponents/Viewer/ListItem.tsx
+++ components/rebuiltComponents/Viewer/ListItem.tsx
@@ -6,14 +6,14 @@ function ListItem({ item }: { item: track }) {
 		<div className='flex flex-row gap-2 text-sm'>
 			<p
 				onClick={() => window.open(item.url, '_blank')?.focus()}
-				className='cursor-pointer bg-slate-500/50 rounded'
+				className='cursor-pointer rounded bg-slate-500/10'
 			>
 				{item.title}: <em>{item.artist}</em>
 			</p>
-			<button className='bg-slate-500/50 rounded'>
+			<button className='rounded bg-slate-500/10'>
 				<p>Copy</p>
 			</button>
-			<button className='bg-slate-500/50 rounded'>
+			<button className='rounded bg-slate-500/10'>
 				<p>Add</p>
 			</button>
 		</div>
