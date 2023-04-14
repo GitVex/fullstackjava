8beef10969875cf09c4cb01189ef49a4e93dc22c
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-14 08:13:03 +0000

    refactor ListItem

--- components/rebuiltComponents/Viewer/ListItem.tsx
+++ components/rebuiltComponents/Viewer/ListItem.tsx
@@ -4,7 +4,7 @@ import { track } from '@prisma/client';
 function ListItem({ item }: { item: track }) {
 	return (
 		<div className='flex w-full flex-row items-center gap-2 rounded-lg bg-indigo-600/10 p-2 text-sm'>
-			<div className='flex flex-1 flex-col w-full'>
+			<div className='flex flex-1 flex-col w-4/5'>
 				<div
 					onClick={() => window.open(item.url, '_blank')?.focus()}
 					className='w-full cursor-pointer truncate'
