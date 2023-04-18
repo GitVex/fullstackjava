077e8e7ef5b093d80cd66c6e0f06f16f74140c84
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-18 11:36:10 +0000

    dev on `FilterUI`

--- components/rebuiltComponents/Filter/FilterUI.tsx
+++ components/rebuiltComponents/Filter/FilterUI.tsx
@@ -10,7 +10,9 @@ function FilterUI() {
 
 	return (
 		<div className='flex h-full w-full flex-col gap-2 p-6'>
-			<div className='h-1/3 w-full rounded bg-emerald-500' />
+			<div className='h-1/3 w-full rounded bg-emerald-500'>
+				{filterState}
+			</div>
 			<div className='h-1/3 w-full rounded bg-amber-500' />
 			<div className='h-1/3 w-full rounded bg-indigo-500' />
 		</div>
