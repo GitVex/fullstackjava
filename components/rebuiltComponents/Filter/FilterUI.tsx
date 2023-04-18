a52c94cbf3113a98b57c424676b54f23ca1fc294
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-18 11:39:04 +0000

    dev on `FilterUI`

--- components/rebuiltComponents/Filter/FilterUI.tsx
+++ components/rebuiltComponents/Filter/FilterUI.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
 	useFilterState,
 	useFilterStateUpdate,
@@ -8,10 +8,18 @@ function FilterUI() {
 	const filterState = useFilterState();
 	const setFilterState = useFilterStateUpdate();
 
+	useEffect(() => {
+		setFilterState(['foo', 'bar', 'baz']);
+	}, []);
+
 	return (
 		<div className='flex h-full w-full flex-col gap-2 p-6'>
 			<div className='h-1/3 w-full rounded bg-emerald-500'>
-				{filterState}
+				<div className='flex flex-row gap-2'>
+					{filterState.map((tag) => {
+						return <p>{tag}</p>;
+					})}
+				</div>
 			</div>
 			<div className='h-1/3 w-full rounded bg-amber-500' />
 			<div className='h-1/3 w-full rounded bg-indigo-500' />
