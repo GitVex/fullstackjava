9a0d2c1eea7f5384065c4815f29612acf1a79404
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-27 17:12:39 +0000

    refactor: Simplify and optimize OpenAI API call in tagging API
    This commit simplifies and optimizes the OpenAI API call in the tagging API by combining the role and content properties of the messages object, removing redundant code lines, and adding a try-catch block to handle errors. The commit also adds an informative system message with guidelines for formatting tags for youtube video titles.

--- pages/api/tagging.ts
+++ pages/api/tagging.ts
@@ -9,29 +9,23 @@ const openai = new OpenAIApi(configuration);
 
 const tagging = async (req: NextApiRequest, res: NextApiResponse) => {
 
-    const title = req.body.prompt.title;
-    const artist = req.body.prompt.artist;
+    let response;
+    const { title, artist } = req.body.prompt;
 
-    const options = [
-        "Example: 'rock, pop, alternative, indie, alternative rock'",
-        "Try to categorize the tracks into either 'ambience' or 'music', but not both",
-        "Refrain from using more than one word per tag",
-        "do not add special characters to tags like # or @",
-        "translate tags to english.",
-    ]
-    
-    const prompt = 'Please create a raw string without formatting of 5 tags for the given track title: "' + title + '" by "' + artist + '". ' + options.join('. ')
-
-    const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {role: 'user', content: prompt},
-        ],
-        temperature: 0.5,
-        max_tokens: 256,
-    });
-
-    console.log(response);
+    try {
+        const gptResponse = await
+            openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [
+                    { "role": "system", "content": "You are a helpful assistant that likes to tag youtube video titles. Example: 'rock, pop, alternative, indie, alternative rock'. Try to categorize the tracks into either 'ambience', 'music' or 'standalone' Refrain from using more than one word per tag and seperate the tags by ','. Do not add special characters to tags like # or @. Translate tags to english." },
+                    { "role": "user", "content": 'Please create a raw string without formatting of 5 tags for the given track title: "' + title + '" by "' + artist + '". Is it ambience or music?' },
+                ]
+            })
+        response = gptResponse.data.choices[0];
+    }
+    catch (err) {
+        console.log(err);
+    }
 
     res.status(200).json(response);
 }
