d21dc21949c9ffe593cd026259e46aabb00e63db
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-28 20:04:14 +0000

    finally migrated to gpt-4 instead of text-davinci-003

--- pages/api/tagging.ts
+++ pages/api/tagging.ts
@@ -10,9 +10,7 @@ const openai = new OpenAIApi(configuration);
 const tagging = async (req: NextApiRequest, res: NextApiResponse) => {
 
     try {
-
-        console.log('req.body', req.body);
-
+        
         let response;
         const { title, artist } = req.body.prompt;
 
@@ -21,17 +19,16 @@ const tagging = async (req: NextApiRequest, res: NextApiResponse) => {
                 openai.createChatCompletion({
                     model: "gpt-4",
                     messages: [
-                        { "role": "system", "content": "You are a helpful assistant that likes to tag youtube video titles. Example: 'rock, pop, alternative, indie, alternative rock'. Try to categorize the tracks into either 'ambience', 'music' or 'standalone' Refrain from using more than one word per tag and seperate the tags by ','. Do not add special characters to tags like # or @. Translate tags to english." },
-                        { "role": "user", "content": 'Please create a raw string without formatting of 5 tags for the given track title: "' + title + '" by "' + artist + '". Is it ambience or music?' },
+                        { "role": "system", "content": "You are a helpful assistant that likes to tag youtube video titles. Example: 'rock, pop, alternative, indie, alternative rock'. Refrain from using more than one word per tag and seperate the tags by ','. Do not add special characters to tags like # or @. Translate tags to english." },
+                        { "role": "user", "content": "Please create a raw string without formatting of 5 tags for the given track title: '" + title + "' by '" + artist + "'. Try to categorize the tracks into either 'ambience', 'music' or 'standalone'(music & ambience) track? Try to use tags that describe the scene which the track portrays" },
                     ]
                 })
-            response = gptResponse.data.choices[0];
+            response = gptResponse.data.choices[0].message?.content;
         }
         catch (err) {
             console.log('query error', err);
             res.status(500).json(err);
         }
-
         res.status(200).json(response);
 
     } catch (err) {
