68da647ab96f0e4267184ecd15424cea315360a2
GitVex <52197394+GitVex@users.noreply.github.com>
2023-04-28 15:36:39 +0000

    debug

--- pages/api/tagging.ts
+++ pages/api/tagging.ts
@@ -9,26 +9,32 @@ const openai = new OpenAIApi(configuration);
 
 const tagging = async (req: NextApiRequest, res: NextApiResponse) => {
 
-    let response;
-    const { title, artist } = req.body.prompt;
-
     try {
-        const gptResponse = await
-            openai.createChatCompletion({
-                model: "gpt-4",
-                messages: [
-                    { "role": "system", "content": "You are a helpful assistant that likes to tag youtube video titles. Example: 'rock, pop, alternative, indie, alternative rock'. Try to categorize the tracks into either 'ambience', 'music' or 'standalone' Refrain from using more than one word per tag and seperate the tags by ','. Do not add special characters to tags like # or @. Translate tags to english." },
-                    { "role": "user", "content": 'Please create a raw string without formatting of 5 tags for the given track title: "' + title + '" by "' + artist + '". Is it ambience or music?' },
-                ]
-            })
-        response = gptResponse.data.choices[0];
-    }
-    catch (err) {
+
+        let response;
+        const { title, artist } = req.body.prompt;
+
+        try {
+            const gptResponse = await
+                openai.createChatCompletion({
+                    model: "gpt-4",
+                    messages: [
+                        { "role": "system", "content": "You are a helpful assistant that likes to tag youtube video titles. Example: 'rock, pop, alternative, indie, alternative rock'. Try to categorize the tracks into either 'ambience', 'music' or 'standalone' Refrain from using more than one word per tag and seperate the tags by ','. Do not add special characters to tags like # or @. Translate tags to english." },
+                        { "role": "user", "content": 'Please create a raw string without formatting of 5 tags for the given track title: "' + title + '" by "' + artist + '". Is it ambience or music?' },
+                    ]
+                })
+            response = gptResponse.data.choices[0];
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).json({ error: err });
+        }
+
+        res.status(200).json(response);
+
+    } catch (err) {
         console.log(err);
         res.status(500).json({ error: err });
     }
 
-    res.status(200).json(response);
-}
-
-export default tagging;
+    export default tagging;
