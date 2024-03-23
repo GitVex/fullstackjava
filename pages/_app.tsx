b86f721a8d5a5afb3b661868b5d63694281940c0
GitVex <52197394+GitVex@users.noreply.github.com>
2024-03-23 01:32:31 +0000

    cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,6 @@ export default function App({ Component, pageProps }: AppProps) {
 			<WindowSizeProvider>
 				<Component {...pageProps} />
 			</WindowSizeProvider>
-			<SpeedInsights />
 		</>
 	);
 }
